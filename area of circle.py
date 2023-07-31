157f427a713b91c3928472d63f3d2dfefeb7d042
VARSHA101003 <140830697+VARSHA101003@users.noreply.github.com>
2023-07-31 16:59:48 +0530

    Update and rename README.md to area of circle.py

---
+++ area of circle.py
@@ -0,0 +1,8 @@
+# task1
+#write a python program which accepts theradius of a circle from the user and computes area
+#program
+from math import pi
+r = float(input("input the radius of the circle:"))
+print("The area of the circle with radius is:"+str(pi*r**2))
+
+
